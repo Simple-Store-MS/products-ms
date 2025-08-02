7ede5ab0d9204750e26f65998d8e7e570e3aa30f
Robe <roberto.granda@thecodelight.com>
2025-08-01 22:18:51 -0300

    feat: add Dockerfile, .dockerignore, and update environment configuration for NATS support

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,17 +3,25 @@ import * as joi from 'joi';
 
 interface EnvVars {
   DATABASE_URL: string;
+  NATS_SERVERS: string[];
   PORT: number;
 }
 
 const envsSchema = joi
   .object({
     DATABASE_URL: joi.string().required(),
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
     PORT: joi.number().required(),
   })
   .unknown(true);
 
-const result = envsSchema.validate(process.env);
+const result = envsSchema.validate({
+  ...process.env,
+  NATS_SERVERS:
+    process.env.NATS_SERVERS?.split(',').map((server: string) =>
+      server.trim(),
+    ) || [],
+});
 
 if (result.error) {
   throw new Error(`Config validations error ${result.error.message}`);
@@ -23,5 +31,6 @@ const envVars = result.value as EnvVars;
 
 export const envs = {
   databaseUrl: envVars.DATABASE_URL,
+  natsServers: envVars.NATS_SERVERS,
   port: envVars.PORT,
 };
